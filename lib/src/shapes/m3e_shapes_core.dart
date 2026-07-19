b77c0ee1379fd0c6168319b218ca78dd6e16a6ca
Mudit200408 <muditpurohit2004.pm@gmail.com>
2026-07-19 13:25:23 +0530

    feat: Add polygons support in shapes

--- lib/src/shapes/m3e_shapes_core.dart
+++ lib/src/shapes/m3e_shapes_core.dart
@@ -2,3 +2,14 @@ export 'clipper.dart';
 export 'm3e_containers.dart';
 export 'enums/shapes.dart';
 export 'm3e_shapes.dart';
+export 'polygon/shapes.dart'
+    show
+        CornerRounding,
+        Cubic,
+        Matrix4PointTransformer,
+        Morph,
+        MorphToPathExtension,
+        RoundedPolygon,
+        RoundedPolygonToPathExtension,
+        pathFromCubics;
+
