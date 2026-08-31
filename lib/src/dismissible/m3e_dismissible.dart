b621234edef471c079045587bc322f2591bd44a2
Mudit200408 <muditpurohit2004.pm@gmail.com>
2026-08-31 20:10:22 +0530

    dismissible: add multi-action swipe to reveal, spring stretch and direction support

--- lib/src/dismissible/m3e_dismissible.dart
+++ lib/src/dismissible/m3e_dismissible.dart
@@ -3,3 +3,4 @@ export 'm3e_dismissible_card_column.dart';
 export 'sliver_m3e_dismissible_card_list.dart';
 export 'm3e_dismissible_card_controller.dart';
 export 'm3e_dismissible_card_style.dart';
+export 'm3e_swipe_action.dart';
